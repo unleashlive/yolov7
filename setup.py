4682452a9227337371894311482af7df0c63d83c
rod-unleashlive <rodwin.a@unleashlive.com>
2023-03-13 10:55:06 +1100

    fix naming on setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="yolov7_segmentation",
+    name="yolov7_seg",
     version="1.0.0",
     packages=setuptools.find_packages(),
     include_package_data=True,
