1cd9454f87abb77b887dea4a4502fc60cbd64e95
rod-unleashlive <110947080+rod-unleashlive@users.noreply.github.com>
2023-03-23 11:07:02 +1100

    fix monkey_patch on opencv (#2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="yolov7_segmentation",
-    version="1.0.0",
+    version="1.0.1",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[],
